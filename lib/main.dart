c159e0911ca7a7882558107823ee79da5baded48
Jeremiah Akem <agbamajeremiah@gmail.com>
2022-04-05 22:55:05 +0100

    Add custom fonts - Product sans

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Renmoney Task',
       theme: ThemeData(
+        fontFamily: 'Product Sans',
         primarySwatch: const MaterialColor(
           0xFF7765C4,
           AppColors.colorScratch,
