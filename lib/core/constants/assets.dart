9465ba3d2ff5659686d6ba21099bbcd3bdcfd3be
Jeremiah Akem <agbamajeremiah@gmail.com>
2022-04-05 22:31:46 +0100

    Add and register icons assets in Asset class

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,10 @@
+/// [AppAssets] is the class that contains
+/// all the assets used in the app.
+/// It is used to load the assets from the assets folder.
+class AppAssets {
+  ///  [arrowDown] .
+  static const String arrowDown = 'assets/icons/arrow-down.svg';
+
+  ///  [phoneWithArrow] .
+  static const String phoneWithArrow = 'assets/icons/phone-with-arrow.svg';
+}
